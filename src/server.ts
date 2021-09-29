de8356c92d6688a98f4956fc60e84bd4c751c30b
Juliano Alves <contato.julianoalvess@gmail.com>
2021-09-29 10:06:38 -0300

    fix: 🐛 fix port
    fix port

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,52 @@
-import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
-import jwt from 'jsonwebtoken'
-import decode from 'jwt-decode'
-import { generateJwtAndRefreshToken } from './auth';
-import { auth } from './config';
+import cors from "cors";
+import express, { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import decode from "jwt-decode";
+import { generateJwtAndRefreshToken } from "./auth";
+import { auth } from "./config";
 
-import { checkRefreshTokenIsValid, users, seedUserStore, invalidateRefreshToken } from './database';
-import { CreateSessionDTO, DecodedToken } from './types';
+import {
+  checkRefreshTokenIsValid,
+  users,
+  seedUserStore,
+  invalidateRefreshToken,
+} from "./database";
+import { CreateSessionDTO, DecodedToken } from "./types";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 seedUserStore();
 
-function checkAuthMiddleware(request: Request, response: Response, next: NextFunction) {
+function checkAuthMiddleware(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
   const { authorization } = request.headers;
 
   if (!authorization) {
     return response
       .status(401)
-      .json({ error: true, code: 'token.invalid', message: 'Token not present.' })
+      .json({
+        error: true,
+        code: "token.invalid",
+        message: "Token not present.",
+      });
   }
 
-  const [, token] = authorization?.split(' ');
+  const [, token] = authorization?.split(" ");
 
   if (!token) {
-    return response 
+    return response
       .status(401)
-      .json({ error: true, code: 'token.invalid', message: 'Token not present.' })
+      .json({
+        error: true,
+        code: "token.invalid",
+        message: "Token not present.",
+      });
   }
 
   try {
@@ -39,28 +56,39 @@ function checkAuthMiddleware(request: Request, response: Response, next: NextFun
 
     return next();
   } catch (err) {
-
-    return response 
+    return response
       .status(401)
-      .json({  error: true, code: 'token.expired', message: 'Token invalid.' })
+      .json({ error: true, code: "token.expired", message: "Token invalid." });
   }
 }
 
-function addUserInformationToRequest(request: Request, response: Response, next: NextFunction) {
+function addUserInformationToRequest(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
   const { authorization } = request.headers;
 
   if (!authorization) {
     return response
       .status(401)
-      .json({ error: true, code: 'token.invalid', message: 'Token not present.' })
+      .json({
+        error: true,
+        code: "token.invalid",
+        message: "Token not present.",
+      });
   }
 
-  const [, token] = authorization?.split(' ');
+  const [, token] = authorization?.split(" ");
 
   if (!token) {
-    return response 
+    return response
       .status(401)
-      .json({ error: true, code: 'token.invalid', message: 'Token not present.' })
+      .json({
+        error: true,
+        code: "token.invalid",
+        message: "Token not present.",
+      });
   }
 
   try {
@@ -70,30 +98,32 @@ function addUserInformationToRequest(request: Request, response: Response, next:
 
     return next();
   } catch (err) {
-    return response 
+    return response
       .status(401)
-      .json({ error: true, code: 'token.invalid', message: 'Invalid token format.' })
+      .json({
+        error: true,
+        code: "token.invalid",
+        message: "Invalid token format.",
+      });
   }
 }
 
-app.post('/sessions', (request, response) => {
+app.post("/sessions", (request, response) => {
   const { email, password } = request.body as CreateSessionDTO;
 
   const user = users.get(email);
 
   if (!user || password !== user.password) {
-    return response
-      .status(401)
-      .json({ 
-        error: true, 
-        message: 'E-mail or password incorrect.'
-      });
+    return response.status(401).json({
+      error: true,
+      message: "E-mail or password incorrect.",
+    });
   }
 
   const { token, refreshToken } = generateJwtAndRefreshToken(email, {
     permissions: user.permissions,
     roles: user.roles,
-  })
+  });
 
   return response.json({
     token,
@@ -103,41 +133,42 @@ app.post('/sessions', (request, response) => {
   });
 });
 
-app.post('/refresh', addUserInformationToRequest, (request, response) => {
+app.post("/refresh", addUserInformationToRequest, (request, response) => {
   const email = request.user;
   const { refreshToken } = request.body;
 
   const user = users.get(email);
 
   if (!user) {
-    return response
-      .status(401)
-      .json({ 
-        error: true, 
-        message: 'User not found.'
-      });
+    return response.status(401).json({
+      error: true,
+      message: "User not found.",
+    });
   }
 
   if (!refreshToken) {
     return response
       .status(401)
-      .json({ error: true, message: 'Refresh token is required.' });
+      .json({ error: true, message: "Refresh token is required." });
   }
 
-  const isValidRefreshToken = checkRefreshTokenIsValid(email, refreshToken)
+  const isValidRefreshToken = checkRefreshTokenIsValid(email, refreshToken);
 
   if (!isValidRefreshToken) {
     return response
       .status(401)
-      .json({ error: true, message: 'Refresh token is invalid.' });
+      .json({ error: true, message: "Refresh token is invalid." });
   }
 
-  invalidateRefreshToken(email, refreshToken)
+  invalidateRefreshToken(email, refreshToken);
 
-  const { token, refreshToken: newRefreshToken } = generateJwtAndRefreshToken(email, {
-    permissions: user.permissions,
-    roles: user.roles,
-  })
+  const { token, refreshToken: newRefreshToken } = generateJwtAndRefreshToken(
+    email,
+    {
+      permissions: user.permissions,
+      roles: user.roles,
+    }
+  );
 
   return response.json({
     token,
@@ -147,7 +178,7 @@ app.post('/refresh', addUserInformationToRequest, (request, response) => {
   });
 });
 
-app.get('/me', checkAuthMiddleware, (request, response) => {
+app.get("/me", checkAuthMiddleware, (request, response) => {
   const email = request.user;
 
   const user = users.get(email);
@@ -155,14 +186,14 @@ app.get('/me', checkAuthMiddleware, (request, response) => {
   if (!user) {
     return response
       .status(400)
-      .json({ error: true, message: 'User not found.' });
+      .json({ error: true, message: "User not found." });
   }
 
   return response.json({
     email,
     permissions: user.permissions,
     roles: user.roles,
-  })
+  });
 });
 
-app.listen(3333);
+app.listen(process.env.PORT || 3333);
